ade4dfbd160394628470c81be3c703fe9d4e9a81
vivekagg <vivekaggarwal92@gmail.com>
2018-10-11 23:45:45 +0530

    added structure to file system and added chatterbeesearch module

---
+++ ChatterBee/Modules/ChatterBeeSeach/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ChatterBee
+//
+//  Created by Vivek Aggarwal on 10/10/18.
+//  Copyright © 2018 vivek. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController : UIViewController,UISearchBarDelegate,UITableViewDelegate,UITableViewDataSource  {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+}
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        if (self.searchBar.text != nil){
+            print(self.searchBar.text!)
+        }
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
+
---
+++ ChatterBee/Network.swift
@@ -0,0 +1,11 @@
+//
+//  Network.swift
+//  ChatterBee
+//
+//  Created by Vivek Aggarwal on 10/10/18.
+//  Copyright © 2018 vivek. All rights reserved.
+//
+
+import Alamofire
+
+
